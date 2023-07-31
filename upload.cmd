8d545a83d4ddc74bee82ed4f278af025589d6556
96coder <mmcoder96@gmail.com>
2023-08-01 00:44:04 +0630

    api khoe yin kmkl

--- upload.cmd
+++ upload.cmd
@@ -1,8 +1,10 @@
 @echo off
 echo ========== Upload ================
+echo.
 git add .
 git commit -m "api khoe yin kmkl"
 git push -u origin main
+echo.
 echo ========== Success ===============
 echo.
 echo Press a key to close.
